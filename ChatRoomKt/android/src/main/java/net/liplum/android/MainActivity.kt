b1323c64ff3b4ca03e7505167d1a4f874013dd5f
liplum <Li_plum@outlook.com>
2022-06-13 20:44:53 +0800

    Init chat room app with Compose UI.

---
+++ ChatRoomKt/android/src/main/java/net/liplum/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.liplum.android
+
+import net.liplum.common.App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                App()
+            }
+        }
+    }
+}
---
+++ ChatRoomKt/common/src/desktopMain/kotlin/net/liplum/common/platform.kt
@@ -0,0 +1,5 @@
+package net.liplum.common
+
+actual fun getPlatformName(): String {
+    return "Desktop"
+}
